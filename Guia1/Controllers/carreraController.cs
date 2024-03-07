61d07c484a69f7183621f153103500fb2bd691c8
bryan <bryan@DESKTOP-CQ730LI>
2024-03-07 15:43:22 -0600

    Agregar archivos de proyecto.

---
+++ Guia1/Controllers/carreraController.cs
@@ -0,0 +1,31 @@
+﻿using Guia1.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Guia1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class carreraController : ControllerBase
+    {
+        private readonly equiposContext _equiposContext;
+        public carreraController(equiposContext context)
+        {
+            _equiposContext = context;
+        }
+        [HttpGet]
+        [Route("GetAll")] 
+        public IActionResult Get()
+        {
+            List<carreras> listadoCarreras=(from e in _equiposContext.carreras select e).ToList();
+
+            if (listadoCarreras.Count == 0) return NotFound();
+            return Ok(listadoCarreras);
+
+        }
+    }
+}
+
+
---
+++ Guia1/Models/equiposContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+namespace Guia1.Models
+{
+    public class equiposContext: DbContext
+    {
+        public equiposContext(DbContextOptions<equiposContext>options): base(options) 
+        {
+        
+        }
+        public DbSet<carreras> carreras { get; set; }
+    }
+}
